12cd3eb2a69e71ceef449caa02ac2f8f23c2869d
panchalom78 <panchalom787@gmail.com>
2025-03-18 21:30:33 +0530

    Refactor CORS configuration in server/index.js to use an array for allowed origins and update vercel.json to streamline routing setup, ensuring proper request handling and compatibility.

--- server/index.js
+++ server/index.js
@@ -15,9 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
-        allowedHeaders: ["Content-Type", "Authorization"],
-        
+        origin: [process.env.FRONTEND_URL],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         credentials: true,
     })
