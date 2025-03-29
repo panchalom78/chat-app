42823a91075249dfbd1b9fd3d60c7d3b9320947b
panchalom78 <panchalom787@gmail.com>
2025-03-29 21:01:00 +0530

    Update cookie SameSite attribute to 'none' for cross-site usage

--- server/lib/util.js
+++ server/lib/util.js
@@ -13,7 +13,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
     });
     return token;
 };
